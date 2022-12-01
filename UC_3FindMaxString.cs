a9cfffcc241ae1f1945f150715c523100f868ddd
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-12-01 23:23:16 +0530

    Added refactor code program

---
+++ UC_3FindMaxString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingMaxUsingGenerics_13
+{
+    internal class UC_3FindMaxString
+    {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+    }
+}
