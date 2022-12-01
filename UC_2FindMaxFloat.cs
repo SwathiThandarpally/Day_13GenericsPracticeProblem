fb7d369442433169a66259d4dbdf2f767e1132dc
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-12-01 23:14:22 +0530

    Added finding max float progam

---
+++ UC_2FindMaxFloat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingMaxUsingGenerics_13
+{
+    internal class UC_2FindMaxFloat
+    {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+    }
+}
